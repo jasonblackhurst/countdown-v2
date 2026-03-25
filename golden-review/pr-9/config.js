33caba8442927512a0f836f884f5e4a7369855d7
jasonblackhurst <jasonblackhurst@users.noreply.github.com>
2026-03-25 03:39:27 +0000

    deploy: db011d3303a936d1bb43d1d52b2ccac2b4df1d13

---
+++ golden-review/pr-9/config.js
@@ -0,0 +1,26 @@
+var GOLDEN_CONFIG = {
+  pr: 9,
+  images: [
+    {
+      name: 'home_screen_desktop',
+      testImage: 'home_screen_desktop_testImage.png',
+      masterImage: 'home_screen_desktop_masterImage.png',
+      maskedDiff: 'home_screen_desktop_maskedDiff.png',
+      isolatedDiff: 'home_screen_desktop_isolatedDiff.png',
+    },
+    {
+      name: 'home_screen_iphone_14_pro',
+      testImage: 'home_screen_iphone_14_pro_testImage.png',
+      masterImage: 'home_screen_iphone_14_pro_masterImage.png',
+      maskedDiff: 'home_screen_iphone_14_pro_maskedDiff.png',
+      isolatedDiff: 'home_screen_iphone_14_pro_isolatedDiff.png',
+    },
+    {
+      name: 'home_screen_iphone_se',
+      testImage: 'home_screen_iphone_se_testImage.png',
+      masterImage: 'home_screen_iphone_se_masterImage.png',
+      maskedDiff: 'home_screen_iphone_se_maskedDiff.png',
+      isolatedDiff: 'home_screen_iphone_se_isolatedDiff.png',
+    },
+  ]
+};
