45a5810dd6834854228966f36418df9fcc2982fe
jasonblackhurst <jasonblackhurst@users.noreply.github.com>
2026-03-25 03:49:21 +0000

    deploy: f1a7f1d39bcfe72b68a9cb52f912d7345043aa76

---
+++ golden-review/pr-14/config.js
@@ -0,0 +1,47 @@
+var GOLDEN_CONFIG = {
+  pr: 14,
+  images: [
+    {
+      name: 'game_screen_midround_desktop',
+      testImage: 'game_screen_midround_desktop_testImage.png',
+      masterImage: 'game_screen_midround_desktop_masterImage.png',
+      maskedDiff: 'game_screen_midround_desktop_maskedDiff.png',
+      isolatedDiff: 'game_screen_midround_desktop_isolatedDiff.png',
+    },
+    {
+      name: 'game_screen_midround_iphone_14_pro',
+      testImage: 'game_screen_midround_iphone_14_pro_testImage.png',
+      masterImage: 'game_screen_midround_iphone_14_pro_masterImage.png',
+      maskedDiff: 'game_screen_midround_iphone_14_pro_maskedDiff.png',
+      isolatedDiff: 'game_screen_midround_iphone_14_pro_isolatedDiff.png',
+    },
+    {
+      name: 'game_screen_midround_iphone_se',
+      testImage: 'game_screen_midround_iphone_se_testImage.png',
+      masterImage: 'game_screen_midround_iphone_se_masterImage.png',
+      maskedDiff: 'game_screen_midround_iphone_se_maskedDiff.png',
+      isolatedDiff: 'game_screen_midround_iphone_se_isolatedDiff.png',
+    },
+    {
+      name: 'home_screen_desktop',
+      testImage: 'home_screen_desktop_testImage.png',
+      masterImage: 'home_screen_desktop_masterImage.png',
+      maskedDiff: 'home_screen_desktop_maskedDiff.png',
+      isolatedDiff: 'home_screen_desktop_isolatedDiff.png',
+    },
+    {
+      name: 'home_screen_iphone_14_pro',
+      testImage: 'home_screen_iphone_14_pro_testImage.png',
+      masterImage: 'home_screen_iphone_14_pro_masterImage.png',
+      maskedDiff: 'home_screen_iphone_14_pro_maskedDiff.png',
+      isolatedDiff: 'home_screen_iphone_14_pro_isolatedDiff.png',
+    },
+    {
+      name: 'home_screen_iphone_se',
+      testImage: 'home_screen_iphone_se_testImage.png',
+      masterImage: 'home_screen_iphone_se_masterImage.png',
+      maskedDiff: 'home_screen_iphone_se_maskedDiff.png',
+      isolatedDiff: 'home_screen_iphone_se_isolatedDiff.png',
+    },
+  ]
+};
