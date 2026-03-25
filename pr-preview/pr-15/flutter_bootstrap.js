0c9e88a2cb3d60583600cf87068a05cf083e9d23
jasonblackhurst <jasonblackhurst@users.noreply.github.com>
2026-03-25 03:40:34 +0000

    deploy: ff042bf1367bbb6cd1adaa05c4c597b00ea14dd3

--- pr-preview/pr-15/flutter_bootstrap.js
+++ pr-preview/pr-15/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4142792543" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3936891227" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
