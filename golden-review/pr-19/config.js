1402d0f07914e979a914f037826b6204a61188d8
jasonblackhurst <jasonblackhurst@users.noreply.github.com>
2026-03-28 16:11:56 +0000

    deploy: 3ce5bc01d6384f3acc75588ba2f781ec79313279

---
+++ golden-review/pr-19/config.js
@@ -0,0 +1,26 @@
+var GOLDEN_CONFIG = {
+  pr: 19,
+  images: [
+    {
+      name: 'lobby_screen_between_rounds_desktop',
+      testImage: 'lobby_screen_between_rounds_desktop_testImage.png',
+      masterImage: 'lobby_screen_between_rounds_desktop_masterImage.png',
+      maskedDiff: 'lobby_screen_between_rounds_desktop_maskedDiff.png',
+      isolatedDiff: 'lobby_screen_between_rounds_desktop_isolatedDiff.png',
+    },
+    {
+      name: 'lobby_screen_between_rounds_iphone_14_pro',
+      testImage: 'lobby_screen_between_rounds_iphone_14_pro_testImage.png',
+      masterImage: 'lobby_screen_between_rounds_iphone_14_pro_masterImage.png',
+      maskedDiff: 'lobby_screen_between_rounds_iphone_14_pro_maskedDiff.png',
+      isolatedDiff: 'lobby_screen_between_rounds_iphone_14_pro_isolatedDiff.png',
+    },
+    {
+      name: 'lobby_screen_between_rounds_iphone_se',
+      testImage: 'lobby_screen_between_rounds_iphone_se_testImage.png',
+      masterImage: 'lobby_screen_between_rounds_iphone_se_masterImage.png',
+      maskedDiff: 'lobby_screen_between_rounds_iphone_se_maskedDiff.png',
+      isolatedDiff: 'lobby_screen_between_rounds_iphone_se_isolatedDiff.png',
+    },
+  ]
+};
